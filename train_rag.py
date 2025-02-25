2764a1605c51cd9ca67f382c80b637bf6dd2780e
zhiyuanyou <zhiyuanyou@foxmail.com>
2025-02-25 23:38:26 +0800

    add base rag training

--- train_rag.py
+++ train_rag.py
@@ -1,22 +1,57 @@
-import torch
-import torch.nn as nn
-import numpy as np
-#from torch.utils.data import DataLoader
-from torch.optim import lr_scheduler
 import clip
-import random
-import time
-from MNL_Loss import Fidelity_Loss, loss_m4, Multi_Fidelity_Loss, Fidelity_Loss_distortion
-import scipy.stats
-from utils_rag import set_dataset, _preprocess2, _preprocess3, convert_models_to_fp32
-import torch.nn.functional as F
-from itertools import product
+import numpy as np
 import os
 import pickle
-from weight_methods import WeightMethods
+import random
+import scipy.stats
+import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from sentence_transformers import SentenceTransformer
+from torch.optim import lr_scheduler
+from utils_rag import set_dataset, _preprocess2, _preprocess3, convert_models_to_fp32
 
-##############################textual template####################################
-qualitys = ['bad', 'poor', 'fair', 'good', 'perfect']
+
+class AlignModel(nn.Module):
+
+    def __init__(self):
+        super(AlignModel, self).__init__()
+        dim_text = 384
+        dim_visual = 512
+        self.temperature = 0.2
+        model, _ = clip.load("ViT-B/32", device=device, jit=False)
+        self.model_visual = model.visual
+        self.model_text = SentenceTransformer("all-MiniLM-L6-v2")
+        self.proj = nn.Linear(dim_text, dim_visual)
+        self.head = nn.Linear(dim_visual, 1)
+
+    def forward(self, img, text, mos):
+        batch_size, num_patch = img.shape[:2]
+        img = img.view(-1, img.size(2), img.size(3), img.size(4))
+        emb_img = self.model_visual(img)  # [B x N, 512]
+        pred = self.head(emb_img)
+        pred = pred.view(batch_size, num_patch, -1).mean(dim=1)
+        loss_mse = ((pred - mos) ** 2).mean()
+
+        emb_text = self.model_text.encode(text, convert_to_tensor=True)  # [B, 384]
+        emb_text = self.proj(emb_text)  # [B, 512]
+        cosine_similarity = F.cosine_similarity(
+            emb_img.unsqueeze(1), emb_text.unsqueeze(0), dim=2
+        )  # [B x N, B]
+        # mean in num_patch dimension
+        cosine_similarity = cosine_similarity.view(batch_size, num_patch, batch_size)
+        cosine_similarity = cosine_similarity.mean(1)  # [B, B]
+        cosine_similarity = cosine_similarity / self.temperature
+        sim_per_img = F.softmax(cosine_similarity, dim=1)
+        sim_per_text = F.softmax(cosine_similarity, dim=0)
+        loss_per_img = -sim_per_img.diagonal().log().mean()
+        loss_per_text = -sim_per_text.diagonal().log().mean()
+        loss_nce = loss_per_img + loss_per_text
+
+        return loss_nce, loss_mse, pred
+
+
 
 ##############################general setup####################################
 img_dir = "/root/Data4ICCV"
@@ -31,15 +66,11 @@ torch.backends.cudnn.benchmark = False
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 initial_lr = 5e-6
-num_epoch = 6
-bs = 64
-
+num_epoch = 10
+num_steps_per_epoch = 1000
+bs = 16
 train_patch = 3
 
-loss_img2 = Fidelity_Loss_distortion()
-loss_scene = Multi_Fidelity_Loss()
-
-joint_texts = torch.cat([clip.tokenize(f"a photo with {c} quality") for c in qualitys]).to(device)
 
 ##############################general setup####################################
 
@@ -48,52 +79,28 @@ preprocess3 = _preprocess3()
 
 opt = 0
 def freeze_model(opt):
-    model.logit_scale.requires_grad = False
-    if opt == 0: #do nothing
+    if opt == 0: # do nothing
         return
     elif opt == 1: # freeze text encoder
-        for p in model.token_embedding.parameters():
-            p.requires_grad = False
-        for p in model.transformer.parameters():
-            p.requires_grad = False
-        model.positional_embedding.requires_grad = False
-        model.text_projection.requires_grad = False
-        for p in model.ln_final.parameters():
+        for p in model.model_text.parameters():
             p.requires_grad = False
     elif opt == 2: # freeze visual encoder
-        for p in model.visual.parameters():
+        for p in model.model_visual.parameters():
             p.requires_grad = False
     elif opt == 3:
         for p in model.parameters():
-            p.requires_grad =False
+            p.requires_grad = False
 
-
-def do_batch(x, text):
-    batch_size = x.size(0)
-    num_patch = x.size(1)
-
-    x = x.view(-1, x.size(2), x.size(3), x.size(4))
-
-    logits_per_image, logits_per_text = model.forward(x, text)
-
-    logits_per_image = logits_per_image.view(batch_size, num_patch, -1)
-    logits_per_text = logits_per_text.view(-1, batch_size, num_patch)
-
-    logits_per_image = logits_per_image.mean(1)
-    logits_per_text = logits_per_text.mean(2)
-
-    logits_per_image = F.softmax(logits_per_image, dim=1)
-
-    return logits_per_image, logits_per_text
 
 def train(model, best_result, best_epoch, srcc_dict):
     start_time = time.time()
     beta = 0.9
     running_loss = 0 if epoch == 0 else train_loss[-1]
     running_duration = 0.0
-    num_steps_per_epoch = 200
     local_counter = epoch * num_steps_per_epoch + 1
+    convert_models_to_fp32(model)
     model.eval()
+
     loaders = []
     for loader in train_loaders:
         loaders.append(iter(loader))
@@ -105,6 +112,7 @@ def train(model, best_result, best_epoch, srcc_dict):
     for step in range(num_steps_per_epoch):
         #total_loss = 0
         all_batch = []
+        text_batch = []
         gmos_batch = []
         num_sample_per_task = []
 
@@ -116,41 +124,26 @@ def train(model, best_result, best_epoch, srcc_dict):
                 sample_batched = next(loader)
                 loaders[dataset_idx] = loader
 
-            x, gmos = sample_batched['I'], sample_batched['mos']
-
+            x, text, gmos = sample_batched['I'], sample_batched["text"], sample_batched['mos']
             x = x.to(device)
+            all_batch.append(x)
+            text_batch += text
             gmos = gmos.to(device)
             gmos_batch.append(gmos)
             num_sample_per_task.append(x.size(0))
 
-            # preserve all samples into a batch, will be used for optimization of scene and distortion type later
-            all_batch.append(x)
+        all_batch = torch.cat(all_batch, dim=0)  # [128, 3, 3, 224, 224]
+        gmos_batch = torch.cat(gmos_batch, dim=0)  # [128, ]
 
-        all_batch = torch.cat(all_batch, dim=0)
-        gmos_batch = torch.cat(gmos_batch, dim=0)
+        loss_nce, loss_mse, _ = model(all_batch, text_batch, gmos_batch)
+        loss = loss_nce + loss_mse
 
         optimizer.zero_grad()
-        logits_per_image, _ = do_batch(all_batch, joint_texts)
-
-        logits_per_image = logits_per_image.view(-1, len(qualitys))
-        logits_quality = 1 * logits_per_image[:, 0] + 2 * logits_per_image[:, 1] + 3 * logits_per_image[:, 2] + \
-                         4 * logits_per_image[:, 3] + 5 * logits_per_image[:, 4]
-
-        total_loss = loss_m4(logits_quality, num_sample_per_task, gmos_batch.detach()).mean()
-
-        total_loss = total_loss
-
-        total_loss.backward()
-
-        if device == "cpu":
-            optimizer.step()
-        else:
-            convert_models_to_fp32(model)
-            optimizer.step()
-            clip.model.convert_weights(model)
+        loss.backward()
+        optimizer.step()
 
         # statistics
-        running_loss = beta * running_loss + (1 - beta) * total_loss.data.item()
+        running_loss = beta * running_loss + (1 - beta) * loss.data.item()
         loss_corrected = running_loss / (1 - beta ** local_counter)
 
         current_time = time.time()
@@ -170,7 +163,7 @@ def train(model, best_result, best_epoch, srcc_dict):
 
     all_result = {'val':{}, 'test':{}}
     if (epoch >= 0):
-        srcc = eval(test_loaders, phase='val', dataset='koniq10k')
+        srcc = eval(test_loader, phase='val', dataset='koniq10k')
         print('**********New overall results!**********')
         best_epoch = epoch
         best_result = srcc
@@ -182,51 +175,37 @@ def train(model, best_result, best_epoch, srcc_dict):
             'epoch': epoch,
             'model_state_dict': model.state_dict(),
             'optimizer_state_dict': optimizer.state_dict(),
-            'all_results':all_result
+            'all_results': all_result
         }, ckpt_name)  # just change to your preferred folder/filename
 
     return best_result, best_epoch, srcc_dict, all_result
 
 
-def eval(test_loaders, phase, dataset):
+def eval(test_loader, phase, dataset):
     model.eval()
     q_mos = []
-    q_hat = []
-    loaders = []
-    for loader in test_loaders:
-        loaders.append(iter(loader))
+    q_pred = []
 
-    for step, sample_batched in enumerate(loader, 0):
-
-        x, gmos = sample_batched['I'], sample_batched['mos']
+    for step, sample_batched in enumerate(test_loader, 0):
+        x, text, gmos = sample_batched['I'], sample_batched["text"], sample_batched['mos']
 
         x = x.to(device)
-        #q_mos.append(gmos.data.numpy())
+        gmos = gmos.to(device)
         q_mos = q_mos + gmos.cpu().tolist()
-
-        # Calculate features
         with torch.no_grad():
-            logits_per_image, _ = do_batch(x, joint_texts)
+            _, _, pred = model(x, text, gmos)
+        q_pred = q_pred + pred.squeeze(1).cpu().tolist()
 
-        logits_per_image = logits_per_image.view(-1, len(qualitys))
-
-        logits_quality = logits_per_image
-
-
-        quality_preds = 1 * logits_quality[:, 0] + 2 * logits_quality[:, 1] + 3 * logits_quality[:, 2] + \
-                        4 * logits_quality[:, 3] + 5 * logits_quality[:, 4]
-
-        q_hat = q_hat + quality_preds.cpu().tolist()
-
-    srcc = scipy.stats.mstats.spearmanr(x=q_mos, y=q_hat)[0]
+    srcc = scipy.stats.mstats.spearmanr(x=q_mos, y=q_pred)[0]
 
     print_text = dataset + ' ' + phase + ' finished'
     print(print_text)
     return srcc
 
+
 num_workers = 8
 for session in range(0,1):
-    model, preprocess = clip.load("ViT-B/32", device=device, jit=False)
+    model = AlignModel().to(device)
 
     optimizer = torch.optim.AdamW(
         model.parameters(), lr=initial_lr,
@@ -243,20 +222,20 @@ for session in range(0,1):
     best_epoch = 0
 
     # avg
-    srcc_dict = {'koniq10k':0.0}
+    srcc_dict = {'koniq10k': 0.0}
 
     spaq_meta = "/root/Data4ICCV/metas/metas_spaq.json"
     liveitw_meta = "/root/Data4ICCV/metas/metas_liveitw.json"
     livefb_meta = "/root/Data4ICCV/metas/metas_livefb.json"
     agiqa_meta = "/root/Data4ICCV/metas/metas_agiqa3k.json"
 
-    spaq_loader = set_dataset(spaq_meta, 32, img_dir, num_workers, preprocess3,
+    spaq_loader = set_dataset(spaq_meta, bs, img_dir, num_workers, preprocess3,
                                               train_patch, False, set=0)
-    liveitw_loader = set_dataset(liveitw_meta, 32, img_dir, num_workers, preprocess3,
+    liveitw_loader = set_dataset(liveitw_meta, bs, img_dir, num_workers, preprocess3,
                                               train_patch, False, set=0)
-    livefb_loader = set_dataset(livefb_meta, 32, img_dir, num_workers, preprocess3,
+    livefb_loader = set_dataset(livefb_meta, bs, img_dir, num_workers, preprocess3,
                                               train_patch, False, set=0)
-    agiqa_loader = set_dataset(agiqa_meta, 32, img_dir, num_workers, preprocess3,
+    agiqa_loader = set_dataset(agiqa_meta, bs, img_dir, num_workers, preprocess3,
                                               train_patch, False, set=0)
 
     koniq_meta = "/root/Data4ICCV/metas/metas_koniq.json"
@@ -264,11 +243,13 @@ for session in range(0,1):
                                              15, True, set=2)
 
     train_loaders = [spaq_loader, liveitw_loader, livefb_loader, agiqa_loader]
-    test_loaders = [koniq_loader]
+    test_loader = koniq_loader
 
     result_pkl = {}
     for epoch in range(0, num_epoch):
-        best_result, best_epoch, srcc_dict, all_result = train(model, best_result, best_epoch, srcc_dict)
+        best_result, best_epoch, srcc_dict, all_result = train(
+            model, best_result, best_epoch, srcc_dict
+        )
         scheduler.step()
 
         result_pkl[str(epoch)] = all_result
